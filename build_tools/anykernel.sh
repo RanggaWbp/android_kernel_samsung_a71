fd59766aed12c7fe0ba396debf94409977381c5a
RanggaWbp <ranggawbp16@gmail.com>
2026-09-17 10:45:07 +0700

    fix(ci): align AnyKernel3 packaging with Boot Header v2 and eBPF legacy patch
    - Separate raw Image and sdmmagpie.dtb
    - Add dynamic android.legacy_ebpf=1 cmdline patching for Android < 14
    - Add automatic OneUI vs AOSP mode detection

--- build_tools/anykernel.sh
+++ build_tools/anykernel.sh
@@ -1,6 +1,7 @@
-### AnyKernel3 Setup for Samsung Galaxy SM7150
+### AnyKernel3 Ramdisk Mod Script
 ## Modified for ReSukiSU by RanggaWbp
 
+### AnyKernel setup
 properties() { '
 kernel.string=Custom Kernel Samsung A71 And M51 Support ReSukiSU by RanggaWbp Menerima Pembuatan Kernel WA: 085806047666
 do.devicecheck=1
@@ -8,32 +9,60 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=a71
-device.name2=a71q
-device.name3=m51
-device.name4=m51q
+device.name1=m51
+device.name2=m51q
+device.name3=a71
+device.name4=a71q
 device.name5=sm7150
 supported.versions=10 - 16
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; }
 
-# Deteksi otomatis partisi boot Samsung SM7150
-BLOCK=auto;
-[ -e /dev/block/by-name/boot ] && BLOCK=/dev/block/by-name/boot;
-[ -e /dev/block/bootdevice/by-name/boot ] && BLOCK=/dev/block/bootdevice/by-name/boot;
-block=$BLOCK;
+# Boot shell variables
+block=/dev/block/bootdevice/by-name/boot;
+[ ! -e "$block" ] && block=/dev/block/by-name/boot;
 
-IS_SLOT_DEVICE=0;
 is_slot_device=0;
-RAMDISK_COMPRESSION=auto;
 ramdisk_compression=auto;
-PATCH_VBMETA_FLAG=auto;
 patch_vbmeta_flag=auto;
 
-# Import library inti AnyKernel3
+# Import functions/variables
 . tools/ak3-core.sh;
 
-# Inisialisasi, injeksi kernel Image.gz-dtb, dan penulisan partisi boot
+# Boot install
 dump_boot;
+
+# Deteksi versi Android & jenis ROM klien
+device=$(file_getprop /system/build.prop ro.product.system.device);
+android=$(file_getprop /system/build.prop ro.build.version.sdk);
+oneui=$(file_getprop /system/build.prop ro.build.version.oneui);
+
+patch_cmdline "android.is_aosp" "";
+
+# Wajib untuk Android 10, 11, 12, dan 13 agar tidak panic eBPF
+if [ "$android" -lt 34 ]; then
+    ui_print " ";
+    ui_print "Legacy Android detected (SDK $android)! Patching eBPF...";
+    patch_cmdline "android.legacy_ebpf=" "android.legacy_ebpf=1";
+fi
+
+if [ -z "$device" ] && [ -z "$android" ]; then
+    ui_print " ";
+    ui_print "Skipping ROM detection...";
+else
+    if [ -n "$oneui" ]; then
+        ui_print " ";
+        ui_print "OneUI ROM detected!";
+    elif [ "$device" = "generic" ]; then
+        ui_print " ";
+        ui_print "GSI ROM detected!";
+    else
+        ui_print " ";
+        ui_print "AOSP ROM detected!";
+        patch_cmdline "android.is_aosp" "android.is_aosp=1";
+    fi
+fi
+
 write_boot;
+## end install
